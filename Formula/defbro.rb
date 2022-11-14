774280cab62e364d66a59dd28d4d72e478fc216e
Joachim Bargsten <919322+jwbargsten@users.noreply.github.com>
2022-11-14 07:50:19 +0000

    defbro: add 0.0.1 bottle.

--- Formula/defbro.rb
+++ Formula/defbro.rb
@@ -5,6 +5,12 @@ class Defbro < Formula
   sha256 "90c5bfa02037cdcdb5356b935298d52d854f3ab1368bc4663b77e99e3287f8a6"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/jwbargsten/misc"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, big_sur: "d61c7ccbf2d3989d4be4102db16c3d6b244ef67f0973df82a250331d62c8c11c"
+  end
+
   depends_on xcode: [">= 11.2", :build]
   depends_on macos: [
     :catalina,
